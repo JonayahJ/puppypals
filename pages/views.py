389a97d3f9d59f6a8c15b0c1aad8bedae83600dc
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-05-31 21:54:23 -0400

    recommitted when completed teams model

---
+++ pages/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Team
+
+# Create your views here.
+
+def home(request):
+    teams = Team.objects.all()
+    data = {
+        "teams": teams,
+    }
+    return render(request, "pages/home.html", data)
+
+def about(request):
+    teams = Team.objects.all()
+    data = {
+        "teams": teams,
+    }
+    return render(request, "pages/about.html", data)
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+def services(request):
+    return render(request, "pages/services.html")
